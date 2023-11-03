8a54391a9616f2717e892bf21427c3a0bb7555f2
hidayry <ryan.hidayat77@gmail.com>
2023-11-03 11:49:36 +0700

    Update small

--- config/awesome/configuration/autostart.lua
+++ config/awesome/configuration/autostart.lua
@@ -4,21 +4,27 @@ local config_dir = filesystem.get_configuration_dir()
 local helpers = require("helpers")
 
 local function autostart_apps()
-	--- Compositor
-	helpers.run.check_if_running("picom", nil, function()
-		awful.spawn("picom --config " .. config_dir .. "configuration/picom.conf -b", false)
-	end)
+    --- Compositor
+    helpers.run.check_if_running("picom", nil, function()
+        awful.spawn("picom --config " .. config_dir ..
+                        "configuration/picom.conf -b", false)
+    end)
 
-	--- Music Server
-	helpers.run.run_once_pgrep("mpd")
-	helpers.run.run_once_pgrep("mpDris2")
-	helpers.run.run_once_pgrep("xrdb merge $HOME/.Xresources")
-  --- Polkit Agent
-	--helpers.run.run_once_ps(
-	--	"polkit-gnome-authentication-agent-1",
-	--	"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-	--)
-	--helpers.run.run_once_grep("nm-applet")
+    --- Music Server
+    helpers.run.run_once_grep("mpd")
+    helpers.run.run_once_grep("mpDris2")
+    --- xrdb
+    helpers.run.run_once_pgrep("xrdb merge $HOME/.Xresources")
+    --- Polkit Agent
+    helpers.run.run_once_pgrep(
+        "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1")
+    -- clipboard
+    helpers.run.run_once_grep("greenclip daemon")
+    -- cursor speed
+    helpers.run.run_once_pgrep("xset r rate 200 50")
+    -- lockscreen
+    helpers.run.run_once_pgrep("xautolock -time 10 -locker '$HOME/.config/awesome/utilities/lock' && echo mem ? /sys/power/state")
+    -- helpers.run.run_once_grep("nm-applet")
 end
 
 autostart_apps()
